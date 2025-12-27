e067abe03e8fb619bb0f9ca48ace4c68aec599a6
AbdellahHrouroud <abdelrd1993@gmail.com>
2025-12-27 18:39:57 +0100

    Add three animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import NavBar from "./Components/NavBar";
 import Hero from "./Components/Hero";
+import Productviewer from "./Components/Productviewer";
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
 	return (
 		<main>
 			<NavBar />
 			<Hero />
+			<Productviewer />
 		</main>
 	);
 };
